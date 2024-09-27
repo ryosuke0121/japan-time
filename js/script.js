dfb10eb05688d870985c6ec4dc4e170ca8e54573
Ryosuke <134034017+ryosuke0121@users.noreply.github.com>
2024-09-27 18:15:47 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let clockX = 0;
+let clockY = 0;
+const easingFactor = 0.1;
+
+// 時計の位置と時間を更新する関数
+function updateClock(event) {
+  const now = new Date();
+  const options = { timeZone: currentTimeZone, hour12: false, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  const formattedTime = now.toLocaleTimeString('ja-JP', options);
+
+  const clock = document.getElementById('clock');
+  clock.textContent = formattedTime;
+
+  const targetX = event.pageX + 10;
+  const targetY = event.pageY + 10;
+
+  const dx = targetX - clockX;
+  const dy = targetY - clockY;
+  clockX += dx * easingFactor;
+  clockY += dy * easingFactor;
+
+  clock.style.left = clockX + 'px';
+  clock.style.top = clockY + 'px';
+
+  requestAnimationFrame(() => {
+    updateClock(event);
+  });
+}
+
+//タイムゾーン設定
+let currentTimeZone = 'Asia/Tokyo';
+
+updateClock({ pageX: 0, pageY: 0 });
+
+document.addEventListener('mousemove', updateClock);
